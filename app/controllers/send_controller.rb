685a0d05df839f432a4b62ad4569bb73ce116118
mike-casas <neneriostb@gmail.com>
2015-02-04 17:39:36 -0500

    form, mailer and controller contact

--- app/controllers/send_controller.rb
+++ app/controllers/send_controller.rb
@@ -1,2 +1,12 @@
 class SendController < ApplicationController
+  def index
+  end
+
+  def create
+      @params= params
+      ContactMailer.contact_send(params).deliver
+      flash[:notice]= "mensaje enviado"
+      redirect_to send_path
+
+  end
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,3 +1,9 @@
 class ContactMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "JOBS Miguel Casas  <contacto@miguelcasas.co>"
+
+   def contact_send(params)
+      @parameters=params
+      mail(to:'ing.miguelcasas@gmail.com',subject:@parameters[:subject])
+   end
+
 end
